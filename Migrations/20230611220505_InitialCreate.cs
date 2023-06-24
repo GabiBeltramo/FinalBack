a3d61d6a9e38c48dd2df4136f72d25abdd0bbe9a
GabiBeltramo <129446969+GabiBeltramo@users.noreply.github.com>
2023-06-24 09:48:31 -0300

    Add files via upload

---
+++ Migrations/20230611220505_InitialCreate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionDeGastos.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consumo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Monto = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    etiquetaId = table.Column<int>(type: "int", nullable: false),
+                    consumoFecha = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consumo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Etiqueta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Etiqueta", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fondo = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consumo");
+
+            migrationBuilder.DropTable(
+                name: "Etiqueta");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
---
+++ Models/CryptoHelper.cs
@@ -0,0 +1,66 @@
+﻿namespace GestionDeGastos.Models
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class CryptoHelper
+    {
+        private static readonly byte[] Key = Encoding.ASCII.GetBytes("UnaClaveSecreta"); // Clave secreta compartida
+
+        public static string Encrypt(string plainText)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Key;
+                byte[] iv = aes.IV;
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(Key, iv), CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter writer = new StreamWriter(cryptoStream))
+                        {
+                            writer.Write(plainText);
+                        }
+                    }
+
+                    byte[] encryptedBytes = memoryStream.ToArray();
+
+                    byte[] combinedBytes = new byte[iv.Length + encryptedBytes.Length];
+                    Buffer.BlockCopy(iv, 0, combinedBytes, 0, iv.Length);
+                    Buffer.BlockCopy(encryptedBytes, 0, combinedBytes, iv.Length, encryptedBytes.Length);
+
+                    return Convert.ToBase64String(combinedBytes);
+                }
+            }
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            byte[] combinedBytes = Convert.FromBase64String(encryptedText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Key;
+                byte[] iv = new byte[aes.IV.Length];
+                byte[] encryptedBytes = new byte[combinedBytes.Length - iv.Length];
+
+                Buffer.BlockCopy(combinedBytes, 0, iv, 0, iv.Length);
+                Buffer.BlockCopy(combinedBytes, iv.Length, encryptedBytes, 0, encryptedBytes.Length);
+
+                using (MemoryStream memoryStream = new MemoryStream(encryptedBytes))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(Key, iv), CryptoStreamMode.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(cryptoStream))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
